d3f6e438022d5a537e6764274d8b29cd504e96e4
DatRatVS <ricorian2006@gmail.com>
2021-05-09 18:14:00 -0300

    1°

---
+++ src/main/java/datrat/codeslogin/codesLoginMain.java
@@ -0,0 +1,14 @@
+package datrat.codeslogin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class codesLoginMain extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
