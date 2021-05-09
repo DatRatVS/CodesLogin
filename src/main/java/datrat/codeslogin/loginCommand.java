f7683f3b33aa1fee0755a9c510da4a6bcb046600
DatRatVS <ricorian2006@gmail.com>
2021-05-09 20:43:25 -0300

    3° Commit

--- src/main/java/datrat/codeslogin/loginCommand.java
+++ src/main/java/datrat/codeslogin/loginCommand.java
@@ -9,39 +9,41 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 
-// a
 public class loginCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("register")) {
 
-			// Checa se quem mandou o comando foi o console. Caso sim, ele manda uma mensagem.
+			// Checa se quem mandou o comando foi o console. Caso sim, ele manda uma mensagem,
 			// que diz que o console não pode usar o comando.
 			if (!(sender instanceof Player)) {
 				sender.sendMessage("[Code's Login] - Consoles aren't able to use this command");
 				return true;
 			}
 
-			// Declarando quem é o player castando "(Player) sender".
-			// Assim indicando quem mandou o comando.
+			// Declarando quem é o player castando "(Player) sender",
+			// assim indicando quem mandou o comando.
 			Player player = (Player) sender;
 
-			// Detecção se o player mandou o comando puro.
-			// O ensina a usar o comando de maneira correta.
+			// Detecção se o player mandou o comando puro, caso sim
+			// o ensina a usar o comando de maneira correta.
 			if (args.length == 0) {
 				player.sendMessage(ChatColor.GOLD + "" + "[Code's Login] - Usage: /register (password)");
 				return true;
 			}
 
-			// Coloca o argumento que o player indicou,
-			// Na hashmap.
-			passwords.put(player, args[0]);
+			// Coloca o argumento que o player indicou
+			// na hashmap.
+			passMap.put(player, args[0]);
+			player.sendMessage(ChatColor.GOLD + "" + "[Code's Login] - You are registered successfully");
 			return true;
 		}
+
+		// Retorna falso ao executor do comando caso algo de errado
+		// magicamente aconteça.
 		return false;
 	}
 
-
 	// Hashmap onde guarda um valor "String".
 	// No qual seria a senha do usuário.
-	public HashMap<Player, String> passwords = new HashMap<>();
+	public HashMap<Player, String> passMap = new HashMap<>();
 }
