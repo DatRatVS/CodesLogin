bbdae0d4550a9c7785ad04f06181b5635eaca5e5
DatRatVS <ricorian2006@gmail.com>
2021-05-09 18:30:12 -0300

    2°

--- src/main/java/datrat/codeslogin/codesLoginMain.java
+++ src/main/java/datrat/codeslogin/codesLoginMain.java
@@ -2,12 +2,23 @@ package datrat.codeslogin;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class codesLoginMain extends JavaPlugin {
 
+	// Declarando a instancia do plugin pra caso precise.
+	public static codesLoginMain instance;
+
+	// Sequencia de inicialização do plugin.
 	@Override
 	public void onEnable() {
+
+		// Declara que o comando "/register" existe na classe loginCommand.class
+		Objects.requireNonNull(this.getCommand("register")).setExecutor(new loginCommand());
+
 	}
 
+	// Sequencia de desligamento do plugin.
 	@Override
 	public void onDisable() {
 	}
